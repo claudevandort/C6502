73579e14dcb656d0025a1f2ac3bee5d7b38add50
Claudio Guerra <claudio.guerra@gomoder.com>
2024-04-16 14:50:38 +0800

    fix: make test now fails on failed tests

--- tests/main.c
+++ tests/main.c
@@ -12,6 +12,9 @@ int main() {
 
   CU_basic_set_mode(CU_BRM_VERBOSE);
   CU_basic_run_tests();
+
+  int failures = CU_get_number_of_failures();
   CU_cleanup_registry();
-  return CU_get_error();
+
+  return (failures == 0) ? 0 : -1;
 }
